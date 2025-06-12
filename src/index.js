be523dff4390f8046e60499222c57ace3a4f5e6f
Maugrins <shadowboxartsstore@gmail.com>
2025-06-12 15:22:36 -0700

    2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Header from './Header';
+import Header from './Header.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
