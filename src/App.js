fb5c08be31b8414e7f010dcebe6609c15f3a581c
Maugrins <shadowboxartsstore@gmail.com>
2025-06-12 13:52:53 -0700

    3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import logo from './smirk.png';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-      Mau (A CAT) is a breed of domestic cat that originated in Egypt. Known for their striking spotted coat, Mau cats are known for their playful and affectionate nature. They are also recognized for their unique ability to jump high and run fast, making them agile hunters. The breed is characterized by its large almond-shaped eyes, which can come in various colors, and its distinctive "M" marking on the forehead. Mau cats are social animals that thrive on interaction with their human companions and can be quite vocal. They are also known for their intelligence and curiosity, often exploring their surroundings with great interest.
-      </p>
-      <p>
-      Grins (TO SMILE IN AN UNRESTRAINED MANNER) is a facial expression characterized by the upward curving of the corners of the mouth, often accompanied by a showing of teeth. It is typically associated with feelings of happiness, amusement, or pleasure. Grinning can vary in intensity from a subtle smile to a broad, toothy grin. It is a universal expression that transcends cultural boundaries and is often used as a form of non-verbal communication to convey friendliness or approval. Grinning can also be contagious, prompting others to smile in return.
-      </p>
-      </header>
-      <body>
-     <p>Welcome to the shadowBox</p>
-      </body>
       <footer className="App-footer">
         public communication: shadowboxstore@gmail.com
       </footer>
